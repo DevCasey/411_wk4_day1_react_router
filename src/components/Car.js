baee8ce17fdee1b36ccba979e515bf94595b56ed
Casey Calkins <c.angelcalkins@outlook.com>
2020-02-03 22:20:08 -0600

    Casey - Finished

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import cars from '../cars.json'
+import {Container, Paper, Chip} from '@material-ui/core';
 // import material ui components here //
 // Container, Paper, Chip //
 
 const Car = (props) => {
+    
+    const id = props.match.params.id
+    const car = cars.find(e => e.id == id)
+
     return (
-        <h1>A specific car</h1>
+        
+        <Container maxWidth="300px" className="car-outter">
+            <Paper className="paper-car">
+                <h1>{car.Name}</h1>
+                {Object.keys(car).map((key, index) => {
+                    return <Chip label={`${key}: ${car[key]}`}></Chip>
+                })}
+            </Paper>
+        </Container>
     )
 }
 
